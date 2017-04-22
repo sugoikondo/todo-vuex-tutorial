2b3dcb88171aa22a7b28a470e79fe78b57938e28
atsu <atsu9615@me.com>
2017-04-22 17:01:09 +0900

    [doing]TODOリストの作成

---
+++ src/components/TodoCard.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="card">
+  <div class="card-content">
+    <div class="card-title">{{todo.name}}</div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: Object
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 })
