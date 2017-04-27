df0436c32a2e4912f18e26ae85cd8254cf5b0b29
atsu <atsu9615@me.com>
2017-04-27 18:53:48 +0900

    menu add

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,8 +1,28 @@
 <template>
 <md-card>
   <md-card-header>
-    <div class="md-title">{{todo.name}}</div>
-    <div class="md-subhead">作成日: {{todo.createdAt}}</div>
+    <md-card-header-text>
+      <div class="md-title">{{todo.name}}</div>
+      <div class="md-subhead">作成日: {{todo.createdAt}}</div>
+
+    </md-card-header-text>
+    <md-menu md-size="4" md-direction="bottom left">
+      <md-button class="md-icon-button" md-menu-trigger>
+        <md-icon>more_vert</md-icon>
+      </md-button>
+
+      <md-menu-content>
+        <md-menu-item>
+          <md-icon>close</md-icon>
+          <span>削除</span>
+        </md-menu-item>
+
+        <md-menu-item>
+          <md-icon>edit</md-icon>
+          <span>編集</span>
+        </md-menu-item>
+      </md-menu-content>
+    </md-menu>
   </md-card-header>
 </md-card>
 </template>
